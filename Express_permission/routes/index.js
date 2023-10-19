01b58c7ee4d3fc9b7fc60e41788d37f65270bb99
User <user@MacBook-Pro-cua-User.local>
2023-10-19 15:31:34 +0700

    update code

--- Express_permission/routes/index.js
+++ Express_permission/routes/index.js
@@ -1,8 +1,42 @@
 var express = require("express");
 var router = express.Router();
+const model = require("../models/index");
+const User = model.User;
+const Role = model.Role;
+const Permission = model.Permission;
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", async function (req, res, next) {
+	if (req.user) {
+		const { id } = req.user;
+
+		const user = await User.findOne({
+			where: {
+				id,
+			},
+
+			include: {
+				model: Role,
+			},
+		});
+
+		const roles = user.Roles;
+
+		let permissions = await Promise.all(
+			roles.map(async ({ id }) => {
+				const role = await Role.findOne({
+					where: { id },
+					include: {
+						model: Permission,
+					},
+				});
+				return role.Permissions;
+			})
+		);
+
+		permissions = permissions.map(({ value }) => value);
+		console.log(permissions);
+	}
 	res.render("index", { title: "Express" });
 });
 
