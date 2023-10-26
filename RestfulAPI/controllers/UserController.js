f91e2c3111a81e137496ccd515b34be1000ebdcb
User <user@192.168.2.193>
2023-10-26 09:01:52 +0700

    update code

--- RestfulAPI/controllers/UserController.js
+++ RestfulAPI/controllers/UserController.js
@@ -5,83 +5,83 @@ const { response } = require("express");
 const User = model.User;
 
 module.exports = {
-    index: async (req, res) => {
-        const {
-            query,
-            order = "asc",
-            sort = "createdAt",
-            limit,
-            page = 1,
-        } = req.query;
-        const options = {
-            attributes: ["id", "name", "email", "createdAt", "updatedAt"],
-            order: [[sort, order]],
-        };
-        if (query) {
-            options.where = {
-                [Op.or]: [
-                    {
-                        name: {
-                            [Op.like]: `%${query}%`,
-                        },
-                    },
-                ],
-            };
-        }
+	index: async (req, res) => {
+		const {
+			query,
+			order = "asc",
+			sort = "createdAt",
+			limit,
+			page = 1,
+		} = req.query;
+		const options = {
+			attributes: ["id", "name", "email", "createdAt", "updatedAt"],
+			order: [[sort, order]],
+		};
+		if (query) {
+			options.where = {
+				[Op.or]: [
+					{
+						name: {
+							[Op.like]: `%${query}%`,
+						},
+					},
+				],
+			};
+		}
 
-        if (limit && Number.isInteger(+limit)) {
-            options.limit = +limit;
-            const offset = (page - 1) * limit;
-            options.offset = offset;
-        }
-        const { rows: users, count } = await User.findAndCountAll(options);
-        const response = {
-            status: "success",
-            data: users,
-            count,
-        };
-        res.json(response);
-    },
+		if (limit && Number.isInteger(+limit)) {
+			options.limit = +limit;
+			const offset = (page - 1) * limit;
+			options.offset = offset;
+		}
+		const { rows: users, count } = await User.findAndCountAll(options);
+		const response = {
+			status: "success",
+			data: users,
+			count,
+		};
+		res.json(response);
+	},
 
-    view: async (req, res) => {
-        const { id } = req.params;
-        console.log(id);
-        const user = await User.findByPk(id);
-        if (!user) {
-            res.status(404).json({
-                status: "error",
-                error: "Not Found",
-            });
-            return;
-        }
+	view: async (req, res) => {
+		const { id } = req.params;
+		console.log(id);
+		const user = await User.findByPk(id);
+		if (!user) {
+			res.status(404).json({
+				status: "error",
+				error: "Not Found",
+			});
+			return;
+		}
 
-        res.json({
-            status: "success",
-            data: user,
-        });
-    },
+		res.json({
+			status: "success",
+			data: user,
+		});
+	},
 
-    store: (req, res) => {
-        const { name, email } = req.body;
-        const errors = {};
-        if (!name) {
-            errors.name = "not name";
-        }
-        if (!email) {
-            errors.email = "not email";
-        }
+	store: (req, res) => {
+		const { name, email } = req.body;
+		const errors = {};
+		if (!name) {
+			errors.name = "not name";
+		}
+		if (!email) {
+			errors.email = "not email";
+		}
 
-        const response = {};
+		const response = {};
 
-        if (Object.keys(errors).length) {
-            Object.assign(response, {
-                status: "error",
-                errorText: "Validation",
-                errors,
-            });
-            res.status(400).json(response);
-            return;
-        }
-        res.json({});
-    },
+		if (Object.keys(errors).length) {
+			Object.assign(response, {
+				status: "error",
+				errorText: "Validation",
+				errors,
+			});
+			res.status(400).json(response);
+			return;
+		}
+		res.json({});
+	},
 };
